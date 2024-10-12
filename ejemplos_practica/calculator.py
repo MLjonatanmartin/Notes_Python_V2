089f7b862f74ba7d8b9d601374ade20ff3156844
mljonatanmartin <aimarttin@gmail.com>
2024-10-11 21:04:42 -0500

    Ejemplo_Practica: Primer Programa: Avance en el programa

--- ejemplos_practica/calculator.py
+++ ejemplos_practica/calculator.py
@@ -2,3 +2,19 @@
 Implementarion ofa a basic calculator to perform arithmetic operations
 """
 
+operations = ['+', '-', 'x', '/']
+
+num1, operation, num2 = input("write your operation\n")
+
+def calculator(num1, operation, num2):
+    """
+    docstring
+    """
+    
+    calculation = {
+        "sum": lambda num1, num2: num1 + num2 ,
+
+    }
+
+    if operation in operations:
+        st
