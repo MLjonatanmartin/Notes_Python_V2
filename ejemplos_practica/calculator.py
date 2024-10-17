ced2612a4d72f06d91e5e59b049dd87f67541bbd
mljonatanmartin <aimarttin@gmail.com>
2024-10-16 20:53:41 -0500

    Ejemplo_Practica: Programa Calculadora: Avence en la funcion de la calculadora, hay un bug con el input que no deja hacer operaciones de mas de 2 digitos

--- ejemplos_practica/calculator.py
+++ ejemplos_practica/calculator.py
@@ -1,20 +1,44 @@
 """
-Implementarion ofa a basic calculator to perform arithmetic operations
+Implementarion of a basic calculator to perform arithmetic operations
 """
 
 operations = ['+', '-', 'x', '/']
 
-num1, operation, num2 = input("write your operation\n")
+num_user1, operation, num_user2 = input("write your operation\n")
+
+num1 = int(num_user1)
+num2 = int(num_user2)
 
 def calculator(num1, operation, num2):
-    """
-    docstring
-    """
     
     calculation = {
         "sum": lambda num1, num2: num1 + num2 ,
-
+        "subs": lambda num1, num2: num1 - num2 ,
+        "multi": lambda num1, num2: num1 * num2,
+        "div": lambda num1, num2: num1 / num2
     }
 
     if operation in operations:
-        st
+        
+        if operation == "+":
+            result = calculation["sum"](num1, num2)
+            print(result)
+        
+        elif operation == "-":
+            result = calculation["subs"](num1, num2)
+            print(result)
+
+        elif operation == "x":
+            result = calculation["multi"] (num1, num2)
+            print(result)
+
+        else:
+            if num2 > 0:
+                print("Error, you can't divide number in zero")
+            else:
+                result = calculation["div"] (num1, num2)
+                print(result)
+
+
+calculator(num1, operation, num2)
+
