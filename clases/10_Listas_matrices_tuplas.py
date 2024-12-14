4d7cb08841a5744de62f237b13aa55fdb00ca644
mljonatanmartin <aimarttin@gmail.com>
2024-12-13 19:03:41 -0500

    Clase #10: Listas de más dimensiones y tuplas: Ejemplo de Script con los temas de la clase, script terminado

--- clases/10_Listas_matrices_tuplas.py
+++ clases/10_Listas_matrices_tuplas.py
@@ -32,3 +32,27 @@ print(coordinates[2][1]) # 3
 
 # declaración de una matris con más listas
 
+coordinates_2 = [
+    [[1,1,1],[1,2,1],[1,3,1]],
+    [[2,2,2],[2,1,2],[2,3,2]],
+    [[3,3,3],[3,1,3],[3,2,3]],
+    [[4,4,4],[4,1,4],[4,2,4]],
+]
+
+print(coordinates_2)
+
+# accediendo a un valor especifico de la lista
+
+print(coordinates_2[2][1][0]) # 3
+
+# declaración de una tupla
+
+names = ('luciona', 'tommy', 'vegeta')
+numbers = 1,2,3,4
+word = ('a',)
+
+print(names, numbers, word)
+
+# tipo de dato
+
+print(type(names), type(numbers), type(word))
